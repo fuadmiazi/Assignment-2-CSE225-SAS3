b52733a3eeda3188e2affbedc5f14aed11167f26
fuadmiazi <fuad.hasan.1997@gmail.com>
2018-11-08 04:14:50 +0600

    declared person.h

---
+++ Person.cpp
@@ -0,0 +1,20 @@
+#include "Person.h"
+#include<iostream>
+
+using namespace std;
+
+Person<string>::Person()
+{
+    customerName = NULL;
+    fathersName = NULL;
+    mothersName = NULL;
+    maritalStatus = NULL;
+    nationalId = NULL;
+    spouseName = NULL;
+    presentAddress = NULL;
+    permanentAddress = NULL;
+    height = NULL;
+    weight = NULL;
+    eyeColor = NULL;
+    hairColor = NULL;
+}
--- Person.h
+++ Person.h
@@ -1,6 +1,40 @@
 #ifndef PERSON_H_INCLUDED
 #define PERSON_H_INCLUDED
 
+using namespace std;
 
+class Person
+{
+private:
+    string customerName;
+    string fathersName;
+    string mothersName;
+    string maritalStatus;
+    string nationalId;
+    string spouseName;
+    string presentAddress;
+    string permanentAddress;
+    string height;
+    string weight;
+    string eyeColor;
+    string hairColor;
 
+public:
+    Person();
+    ~Person();
+    string setCustomerName(string);
+    string setFathersName(string);
+    string setMothersName(string);
+    string setMaritalStatus(string);
+    string setNationalId(string);
+    string setSpouseName(string);
+    string setPresentAddress(string);
+    string setPermanentAddress(string);
+    string setHeight(string);
+    string setWeight(string);
+    string setEyeColor(string);
+    string setHairColor(string);
+    void PrintAllInformations();
+
+};
 #endif // PERSON_H_INCLUDED
